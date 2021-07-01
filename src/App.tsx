8ac82876774192aedad0c2fded66bf94ef3268f3
Volodymyr Kozlov <vkozlov@lohika.com>
2021-07-01 19:25:49 +0300

    feature/Create main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, FunctionComponent } from 'react';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FunctionComponent = () => {
+  return <Fragment>
+    <Router>
+      <Container>
+        <Switch>
+          <Redirect to='/'/>
+        </Switch>
+      </Container>
+    </Router>
+  </Fragment>
 }
 
 export default App;
